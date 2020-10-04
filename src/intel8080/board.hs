569816633bbadbdcce1d956b951f88781b037a10
Dr. ERDI Gergo <gergo@erdi.hu>
2020-10-04 17:13:58 +0800

    Start big rewrite of TinyBASIC to make everything clearer

--- src/intel8080/board.hs
+++ src/intel8080/board.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecordWildCards, ApplicativeDo, NumericUnderscores #-}
+{-# LANGUAGE DeriveFunctor, LambdaCase #-}
 {-# LANGUAGE CPP #-}
 
 import Clash.Prelude
@@ -17,13 +18,81 @@ import Data.Maybe
 import Control.Monad
 import Control.Monad.Trans.Maybe
 import Control.Monad.State
+import Data.Bifunctor
+import Data.Bifoldable
+import Data.Bitraversable
 
 createDomain vSystem{vName="Native", vPeriod = hzToPeriod __NATIVE_CLOCK__}
 
 data PortCommand port a
     = ReadPort port
     | WritePort port a
-    deriving (Generic, NFDataX, Show)
+    deriving (Generic, NFDataX, Show, Functor) -- , Bitraversable)
+
+instance Bifunctor PortCommand where
+    {-# INLINE bimap #-}
+    bimap f g = \case
+        ReadPort port -> ReadPort (f port)
+        WritePort port val -> WritePort (f port) (g val)
+
+    {-# INLINE second #-}
+    second = fmap
+
+instance Bifoldable PortCommand where
+    {-# INLINE bifoldMap #-}
+    bifoldMap f g = \case
+        ReadPort port -> f port
+        WritePort port val -> f port <> g val
+
+instance Bitraversable PortCommand where
+    {-# INLINE bitraverse #-}
+    bitraverse f g = \case
+        ReadPort port -> ReadPort <$> f port
+        WritePort port val -> WritePort <$> f port <*> g val
+
+basedAt :: forall n k. (KnownNat n, KnownNat k) => Unsigned (n + k) -> Unsigned (n + k) -> Maybe (Unsigned k)
+basedAt base0 addr = do
+    let (space, offset) = split addr
+    guard $ space == base
+    return offset
+  where
+    (base, _) = split base0
+
+    split :: Unsigned (n + k) -> (Unsigned n, Unsigned k)
+    split = bitCoerce
+
+serialIO
+    :: (KnownNat (ClockDivider dom (HzToPeriod rate)), HiddenClockResetEnable dom)
+    => SNat rate
+    -> Signal dom Bit
+    -> Signal dom (Maybe (PortCommand (Unsigned 1) Value))
+    -> (Signal dom Value, Signal dom Bit)
+serialIO rate rx cmd = (portOut, tx)
+  where
+    inByte = fmap unpack <$> serialRx @8 rate rx
+    outFifo = fifo (fmap pack <$> outByte) txDone
+    (tx, txDone) = serialTx @8 rate outFifo
+
+    (portOut, outByte) = mealyStateB step Nothing (inByte, isNothing <$> outFifo, cmd)
+
+    step (inByte, outReady, cmd) = do
+        traverse (put . Just) inByte
+        case cmd of
+            Just (ReadPort 0x0) -> do
+                inReady <- isJust <$> get
+                let val = (if inReady then 0x01 else 0x00) .|.
+                          (if outReady then 0x02 else 0x00)
+                return (val, Nothing)
+            Just (WritePort 0x0 x) -> do
+                return (0x00, Nothing)
+
+            Just (ReadPort 0x1) -> do
+                queued <- get <* put Nothing
+                return (fromMaybe 0x00 queued, Nothing)
+            Just (WritePort 0x1 x) -> do
+                return (0x00, Just x)
+
+            _ -> return (0x00, Nothing)
 
 topEntity
     :: "CLK"   ::: Clock Native
@@ -53,33 +122,19 @@ topEntity = withEnableGen board
 
         memData = unpack <$> blockRamFile (SNat @0x10000) "image.bin" memAddr memWrite
 
-        dataIn = muxA [ delay Nothing portIn, Just <$> memData ]
+        dataIn = muxA
+            [ delay Nothing portIn
+            , Just <$> memData
+            ]
         interruptRequest = pure False
 
-        inByte = bitCoerce <$> serialRx @8 (SNat @9600) rx
-        outFifo = fifo (bitCoerce <$> outByte) txDone
-        (tx, txDone) = serialTx @8 (SNat @9600) outFifo
+        serCmd = do
+            cmd <- portCmd
+            pure $ bitraverse (basedAt 0xde) pure =<< cmd
 
-        (portIn, outByte) = mealyStateB step Nothing (portCmd, inByte, isNothing <$> outFifo)
-          where
-            step :: (Maybe (PortCommand Port Value), Maybe Value, Bool) -> State (Maybe Value) (Maybe Value, Maybe Value)
-            step (cmd, inByte, outputReady) = do
-                traverse (put . Just) inByte
-                case cmd of
-                    Just (ReadPort 0xde) -> do
-                        inputReady <- isJust <$> get
-                        let val = (if inputReady then 0x01 else 0x00) .|.
-                                  (if outputReady then 0x02 else 0x00)
-                        return (Just val, Nothing)
-                    Just (WritePort 0xde x) -> do
-                        return (Just 0x00, Nothing)
-
-                    Just (ReadPort 0xdf) -> do
-                        queued <- get <* put Nothing
-                        return (Just $ fromMaybe 0x00 queued, Nothing)
-                    Just (WritePort 0xdf x) -> do
-                        return (Just 0x00, Just x)
-
-                    _ -> return (Nothing, Nothing)
+        (serIn, tx) = serialIO (SNat @9600) rx serCmd
+        portIn = muxA
+            [ enable (isJust <$> serCmd) serIn
+            ]
 
 makeTopEntity 'topEntity
