e1a4e0e3432af61fdd80a0bd6a0526d001efe551
Dr. ERDI Gergo <gergo@erdi.hu>
2020-11-16 12:07:08 +0800

    Make `sampleKey` and `printByte` implementations nicer

--- src/Hardware/TinyBASIC/Sim.hs
+++ src/Hardware/TinyBASIC/Sim.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 module Hardware.TinyBASIC.Sim where
 
 import Clash.Prelude hiding (lift)
@@ -9,26 +10,29 @@ import Control.Monad.Trans.Maybe
 import Data.Char (ord, chr, isPrint)
 import System.Terminal
 
-sampleKey :: (MonadInput (TerminalT t m), MonadPrinter (TerminalT t m), MonadPlus m) => TerminalT t m (Maybe (Unsigned 8))
-sampleKey = do
-    flush
-    ev <- awaitWith $ \int ev -> msum
-        [ Just (KeyEvent (CharKey 'C') ctrlKey) <$ int
-        , Just <$> ev
-        , Nothing <$ return ()
-        ]
-    case ev of
-        Just (KeyEvent key mods)
-          | CharKey c <- key, mods == mempty -> return $ Just $ fromIntegral . ord $ c
-          | CharKey 'C' <- key, mods .&. ctrlKey /= mempty -> return $ Just 0x03
-          | CharKey 'D' <- key, mods .&. ctrlKey /= mempty -> lift mzero
-          | EnterKey <- key -> return $ Just 0x0d
-        _ -> return Nothing
+sampleEvent
+    :: (MonadInput m)
+    => m (Maybe (Either Interrupt Event))
+sampleEvent = awaitWith $ \int ev -> msum
+    [ Just . Left <$> int
+    , Just . Right <$> ev
+    , return Nothing
+    ]
+
+sampleKey
+    :: (MonadInput (TerminalT t m), MonadPlus m)
+    => TerminalT t m (Maybe (Unsigned 8))
+sampleKey = sampleEvent >>= \case
+    Just (Left int) -> return $ Just 0x03
+    Just (Right (KeyEvent key mods))
+        | CharKey c <- key, mods == mempty -> return $ Just . fromIntegral . ord $ c
+        | CharKey 'D' <- key, mods .&. ctrlKey /= mempty -> lift mzero
+        | EnterKey <- key -> return $ Just . fromIntegral . ord $ '\r'
+    _ -> return Nothing
+
 
 printByte :: (MonadPrinter m) => Unsigned 8 -> m ()
-printByte val = case val of
-    0x0d -> putStringLn ""
-    _ | isPrint c -> putChar c >> flush
-      | otherwise -> return ()
-  where
-    c = chr . fromIntegral $ val
+printByte val = case chr . fromIntegral $ val of
+    '\r' -> putStringLn ""
+    c | isPrint c -> putChar c
+    _  -> return ()
